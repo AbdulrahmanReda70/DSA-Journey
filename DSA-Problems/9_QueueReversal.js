c4e222f57ffcd0f525b132d393134ba21e03ec2d
Abdulrahman reda <158822881+AbdulrahmanReda70@users.noreply.github.com>
2024-11-04 14:22:51 +0200

    Create 9_QueueReversal.js

---
+++ DSA-Problems/9_QueueReversal.js
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/problems/queue-reversal/1?page=1&category=Queue&sortBy=difficulty
+
+
+class Solution {
+    //Function to reverse the queue.
+    rev(q)
+    {
+        q.arr.reverse()
+       return q;
+    }
+}
